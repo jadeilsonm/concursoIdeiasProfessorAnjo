8bb809486869982859df3eadbf38b853a6e1768f
jadeilson Martinho <jadeilsonm17@gmail.com>
2023-03-25 00:46:26 -0300

    feat: initial project

---
+++ src/main/java/br/edu/ifpe/concursoideiasprofessoranjo/ConcursoIdeiasProfessorAnjoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.concursoideiasprofessoranjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoIdeiasProfessorAnjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcursoIdeiasProfessorAnjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpe/concursoideiasprofessoranjo/ConcursoIdeiasProfessorAnjoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.concursoideiasprofessoranjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoIdeiasProfessorAnjoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
