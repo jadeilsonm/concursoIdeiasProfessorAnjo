68319c1d32e0a88a960bfe8f13b3809b40a8602a
jadeilson Martinho <jadeilsonm17@gmail.com>
2023-03-27 23:16:48 -0300

    fix: dockerfile

--- src/main/java/br/edu/ifpe/concursoideiasprofessoranjo/Configurations/SecurityConfig.java
+++ src/main/java/br/edu/ifpe/concursoideiasprofessoranjo/Configurations/SecurityConfig.java
@@ -1,15 +1,17 @@
 package br.edu.ifpe.concursoideiasprofessoranjo.Configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -18,19 +20,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                    .authorizeRequests()
-                        .antMatchers(HttpMethod.POST, "/api/user")
-                            .permitAll()
-                    .anyRequest()
-                        .authenticated()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity http) throws Exception {
+        http.ignoring().antMatchers("/api/user");
     }
 
 }
