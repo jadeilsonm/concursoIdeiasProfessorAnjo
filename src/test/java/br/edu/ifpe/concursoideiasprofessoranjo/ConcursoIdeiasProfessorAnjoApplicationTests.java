f3525bdc2a143110c6b59ac2d106cb4c2705db11
jadeilson Martinho <jadeilsonm17@gmail.com>
2023-03-27 23:58:16 -0300

    test: unitTest ConcursoIdeiasProfessorAnjoApplicationTests

--- src/test/java/br/edu/ifpe/concursoideiasprofessoranjo/ConcursoIdeiasProfessorAnjoApplicationTests.java
+++ src/test/java/br/edu/ifpe/concursoideiasprofessoranjo/ConcursoIdeiasProfessorAnjoApplicationTests.java
@@ -1,13 +1,22 @@
 package br.edu.ifpe.concursoideiasprofessoranjo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.profiles.active=test"})
 class ConcursoIdeiasProfessorAnjoApplicationTests {
+
+
+    @Autowired
+    private ConcursoIdeiasProfessorAnjoApplication application;
 
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(application);
     }
 
 }
